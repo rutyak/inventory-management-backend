ce50abe961a4960874c1a6cdf443276ebcd0370d
rutyak <rutikkhandekar12@gmail.com>
2025-09-03 02:13:53 +0530

    new-alllow

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://inventory-management-dashboard.netlify.app",
+    "https://inventory-management-frontend-pi-ruby.vercel.app",
   ],
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
